633ce1c88e3c0ab460879da290f6f79e0493df25
yenglikaskar <yenlikaskar00@gmail.com>
2025-01-19 23:55:58 +0500

    second

--- lab1/1.py
+++ lab1/1.py
@@ -1,1 +1,37 @@
+#HOME
 print("Hello World!")
+
+#SYNAX
+if 5 > 2:
+  print("Five is greater than two!")
+
+#COMMENTS
+print("Hello, World!") #This is a comment
+
+#VARIABLES
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#DATA TYPES
+x = 5
+print(type(x))
+
+#NUMBERS
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#CASTING
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#STRINGS
+print("Hello")
+print('Hello')
