9d1d95f1c54862fda4080453c0449bf9dd02c167
yenglikaskar <yenlikaskar00@gmail.com>
2025-01-22 17:48:18 +0500

    q

---
+++ lab1/2.py
@@ -0,0 +1,43 @@
+#BOOLEANS
+print(5 > 3)
+#True
+
+#OPERATORS
+x = 5
+x += 3
+print(x) #8
+
+#LISTS
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[1]) #banana
+
+#TUPLES
+#Use the correct syntax to print the number of items in the fruits tuple.
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+#SETS
+#Which one of these is a set?
+myset = {'apple', 'banana', 'cherry'}
+
+#DICTIONARIES
+#Whis one of theese is a dictionary?
+x = {'type' : 'fruit', 'name' : 'banana'}
+
+#IF..ELSE
+x = 5
+y = 8
+if x > y:
+  print('Hello')
+else:
+  print('Welcome')
+#WELCOME
+
+#WHILE
+#Which statement is a correct syntax to break out of a loop?
+#BREAK
+
+#FOR
+for x in range(3):
+  print(x)
+#0 1 2
