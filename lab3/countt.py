87d9ec2b5bde359107f5f4e9cf3e92d5315a128a
yenglikaskar <yenlikaskar00@gmail.com>
2025-01-27 11:20:42 +0500

    w

---
+++ lab3/countt.py
@@ -0,0 +1,5 @@
+def k(grams):
+    ounces = 28.3495231 * grams
+    print (ounces)
+grams = int(input())
+k(grams)
---
+++ lab3/f.py
@@ -0,0 +1,6 @@
+def fa(f):
+    c = (5 / 9) * (f - 32)
+    print(c)
+
+f = int(input())
+fa(f)
