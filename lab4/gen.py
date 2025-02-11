80d3ab93e46d45ae1fc42066b3bba447d68196e2
yenglikaskar <yenlikaskar00@gmail.com>
2025-02-11 14:36:17 +0500

    q

--- lab4/gen.py
+++ lab4/gen.py
@@ -2,5 +2,17 @@ def gen_sq(n):
     for i in range(n + 1):
         yield i*i
 n = int(input())
+print("squares:")
 for i in gen_sq(n):
+    print( i)
+
+
+def even(k):
+    for i in range(1,k + 1):
+        if (i % 2 == 0):
+            yield i
+k = int(input())
+print("even:")
+for i in even(k):
     print(i)
+
