5f822650a74856c3262036c7bc29964e7cf24c10
yenglikaskar <yenlikaskar00@gmail.com>
2025-02-12 12:17:07 +0500

    q

--- lab4/date.py
+++ lab4/date.py
@@ -2,4 +2,19 @@ from datetime import date, timedelta
 today = date.today()
 print("Today:", today)
 new = today - timedelta(days = 5)
-print("after 5 days", new)
+print("after 5 days ", new)
+
+
+
+yesterday = today - timedelta(days = 1)
+tomorrow = today + timedelta(days = 1)
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
+
+
+from datetime import datetime
+now = datetime.now()
+without = now.replace(microsecond = 0)
+print("With:", now)
+print("Without:", without)
