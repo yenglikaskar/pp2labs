56ae208ab95a401e8be8654e6fd04d6de26f7261
yenglikaskar <yenlikaskar00@gmail.com>
2025-02-19 11:18:36 +0500

    to6

--- lab5/regects.py
+++ lab5/regects.py
@@ -1,5 +1,5 @@
-#1
 import re
+#1
 enter = input("enter: ")
 text = r"ab*"
 match = re.findall(text, enter)
@@ -16,4 +16,23 @@ print(match)
 enter = input("enter ")
 text = r"[a-z]+(?:_[a-z]+)+"
 match = re.findall(text, enter)
+print(match)
+
+#4
+enter = input("enter ")
+text = r"[A-Z][a-z]+"
+match = re.findall(text, enter)
+print(match)
+
+#5
+enter = input("enter ")
+text = r"a.*b$"
+match = re.findall(text, enter)
+print(match)
+
+#6
+enter = input("enter ")
+text = r",|/.|\s"
+replaceto = ":"
+match = re.sub(text, replaceto, enter)
 print(match)
