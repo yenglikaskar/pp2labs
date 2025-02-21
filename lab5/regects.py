00ee4f2bdcab888b87be20575d37ad840d5ac978
yenglikaskar <yenlikaskar00@gmail.com>
2025-02-21 11:59:36 +0500

    done

--- lab5/regects.py
+++ lab5/regects.py
@@ -36,3 +36,28 @@ text = r",|/.|\s"
 replaceto = ":"
 match = re.sub(text, replaceto, enter)
 print(match)
+
+#7
+enter = input("enter ")
+mylist = re.split('_', enter)
+text = mylist[0]
+for i in range(1, len(mylist)):
+    text += mylist[i].capitalize()
+print(text)
+
+#8
+enter = input("enter ")
+match = re.findall('[A-Z][a-z]+', enter)
+print(match)
+
+#9
+enter = input("enter ")
+match = re.sub('([a-z])([A-Z])', r'\1 \2', enter)
+print(match)
+
+#10
+enter = input("enter ")
+match = re.sub('([a-z])([A-Z])', r'\1 \2', enter)
+match1 = re.sub(' ', r'_', match)
+match2 = match1.lower()
+print(match2)
