9b37f0555b2109412e232e37aee755a8434cd108
yenglikaskar <yenlikaskar00@gmail.com>
2025-03-03 04:34:38 +0500

    q

--- lab6/builtin.py
+++ lab6/builtin.py
@@ -1,45 +1,41 @@
-# #1
-# import math
-# def mult(li):
-#     summa = math.prod(li)
-#     print(summa)
-# li = list(map(int, input().split()))
-# mult(li)
+#1
+import math
+def mult(li):
+    summa = math.prod(li)
+    print(summa)
+li = list(map(int, input().split()))
+mult(li)
 
-# #2
-# def countt(text):
-#     upper = sum(1 for a in text if a.isupper())
-#     lower = sum(1 for a in text if a.islower())
-#     print(f"lower case letters:{lower}")
-#     print(f"upper case letters:{upper}")
-# text = input()
-# countt(text)
+#2
+def countt(text):
+    upper = sum(1 for a in text if a.isupper())
+    lower = sum(1 for a in text if a.islower())
+    print(f"lower case letters:{lower}")
+    print(f"upper case letters:{upper}")
+text = input()
+countt(text)
 
-# #3
-# def palin(word):
-#     return word == ''.join(reversed(word))
-# word = input()
-# if palin(word):
-#     print("yes")
-# else:
-#     print("no")
+#3
+def palin(word):
+    return word == ''.join(reversed(word))
+word = input()
+if palin(word):
+    print("yes")
+else:
+    print("no")
 
-# #4
-# import time
-# def square(a):
-#     return math.sqrt(a)
-# a = int(input())
-# b = int(input())
-# time.sleep(b/1000)
-# squa = square(a)
-# print(f"Square root of {a} after {b} miliseconds is {squa}")
+#4
+import time
+def square(a):
+    return math.sqrt(a)
+a = int(input())
+b = int(input())
+time.sleep(b/1000)
+squa = square(a)
+print(f"Square root of {a} after {b} miliseconds is {squa}")
 
 #5
-def istrue(tup):
+def true(tup):
     return all(tup)
-tup = tuple(input().split())
-print(istrue(tup))
-if istrue(tup):
-    print(True)
-else:
-    print(False)
+tup = tuple(map(lambda x: x.lower() == 'true' or x == '1', input().split()))
+print(true(tup))
